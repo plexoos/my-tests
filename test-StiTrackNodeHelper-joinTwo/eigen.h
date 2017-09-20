2a0f3bc7e22926577e9fa745f251cb0d5545c181
Dmitri Smirnov <d.s@plexoos.com>
2017-09-20 11:05:20 -0400

    joinTwo: Ignore first vector component (x) in eigen implementation
    This is done to match the similar trick implemented in the original
    implementation

--- test-StiTrackNodeHelper-joinTwo/eigen.h
+++ test-StiTrackNodeHelper-joinTwo/eigen.h
@@ -49,7 +49,10 @@ double joinTwo(int nP1, const double *P1, const double *E1,
 
   // M = B*(1/(A+B))*X + A*(1/(A+B))*Y
   // EJ_M is the inverse of the sum
-  EJ_m.noalias() = (E1_m + E2_m).llt().solve(M66::Identity());
+  EJ_m.noalias() = (E1_m + E2_m);
+  EJ_m(0, 0) = 1;
+  EJ_m.noalias() = EJ_m.llt().solve(M66::Identity());
+  EJ_m(0, 0) = 0;
 
   // PJ_m = P1_m * EJ_m * E1_m + P2_m * EJ_m * E2_m;
   // PJ_m = E1_m * EJ_m * P1_m.transpose() + E2_m * EJ_m * P2_m.transpose();
