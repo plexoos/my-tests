cab882ebe59ba15fc2d19fe36bd51a3aba6cd311
Dmitri Smirnov <d.s@plexoos.com>
2018-01-09 16:54:07 -0500

    Another helper function to make plotly plot from cvs data file

--- sti-profiling/plotly_csv.py
+++ sti-profiling/plotly_csv.py
@@ -1,5 +1,37 @@
 import pandas
 
+
+def make_plotly_data( csv_filename, legend_format_string=None ):
+    """
+    Example:
+
+       legend_format_string='Trace {: 3d}'
+    """
+
+    dataframe_csv = pandas.read_csv(csv_filename, sep='\s*,\s*', engine='python', index_col=0)
+
+    index_col_values = dataframe_csv.index.values.tolist()
+    head_row_values = dataframe_csv.columns.values.tolist()
+
+    data = []
+    entry_counter = 1
+
+
+    for head_value in head_row_values:
+        trace_name = head_value if legend_format_string is None else legend_format_string.format(entry_counter)
+        trace = {
+                 'x': index_col_values,
+                 'y': dataframe_csv[head_value].tolist(),
+                 'type': 'bar',
+                 'name': trace_name
+                }
+        data.append( trace )
+        entry_counter += 1
+
+    return data
+
+
+
 def make_plotly_obj( csv_filename ):
 
     dataframe_csv = pandas.read_csv(csv_filename, sep='\s*,\s*', engine='python', index_col=0)
